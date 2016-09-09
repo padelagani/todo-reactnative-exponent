66d0a0837c415e111f19d74e507262cefc9be031
padelagani <padelagani1@gmail.com>
2016-09-09 08:19:39 +0530

    Created TaskList Component

---
+++ TaskList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class TaskList extends Component {
+    render() {
+        return(
+            <Text>This is TaskList !</Text>
+        );
+    }
+}
+
+export default TaskList;
